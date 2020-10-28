fb26630112a55ec67582f302e69c74561e7c14ec
Marco Shimomoto <mashimom@gmail.com>
2020-10-28 11:24:58 +0100

    Renamed project files and dirs

--- src/test/java/com/github/mashimom/corona/CoronaNLDataApplicationTests.java
+++ src/test/java/com/github/mashimom/corona/CoronaNLDataApplicationTests.java
@@ -1,10 +1,10 @@
-package com.github.mashimom.mashimomtemplate;
+package com.github.mashimom.corona;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MashimomTemplateApplicationTests {
+class CoronaNLDataApplicationTests {
 
 	@Test
 	void contextLoads() {
