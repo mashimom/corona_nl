f84dbfbb29d22068242558bd3464607cee899a16
Marco Shimomoto <mashimom@gmail.com>
2020-10-28 10:35:18 +0100

    Initial commit

---
+++ src/main/java/com/github/mashimom/mashimomtemplate/MashimomTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.github.mashimom.mashimomtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MashimomTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MashimomTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/mashimom/mashimomtemplate/MashimomTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mashimom.mashimomtemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MashimomTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
